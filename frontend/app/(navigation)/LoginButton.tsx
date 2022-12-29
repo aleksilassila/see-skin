27ed0ca2c1137c635b1253c76fc8c448e45598ca
Aleksi Lassila <aleksi.emil.lassila@gmail.com>
2022-12-29 20:49:25 +0200

    Added decent profile icon

--- frontend/app/(navigation)/LoginButton.tsx
+++ frontend/app/(navigation)/LoginButton.tsx
@@ -2,6 +2,9 @@
 import { useRouter } from "next/navigation";
 import { useUser } from "../user";
 import { AnchorButton, Button } from "../(ui)/Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown, faUser } from "@fortawesome/free-solid-svg-icons";
+import { Popover } from "@headlessui/react";
 
 export default function LoginButton() {
   const router = useRouter();
@@ -12,18 +15,35 @@ export default function LoginButton() {
     router.push("/api/auth/logout");
   }
 
-  if (user?.user) {
+  if (!user?.user) {
     return (
-      <div>
-        <div>Logged in as {user.user?.name}</div>
-        <Button onClick={logOut}>Logout</Button>
-      </div>
+      <AnchorButton intent="secondary" href="/api/auth/google">
+        Login
+      </AnchorButton>
     );
   }
 
   return (
-    <AnchorButton intent="primary" href="/api/auth/google">
-      Login
-    </AnchorButton>
+    <div>
+      <Popover className="relative">
+        <Popover.Button>
+          <div className="flex items-center gap-2 cursor-pointer px-6 h-10 rounded">
+            <FontAwesomeIcon icon={faUser} className="h-4" />
+            <FontAwesomeIcon icon={faChevronDown} className="h-4" />
+          </div>
+        </Popover.Button>
+
+        <Popover.Panel className="absolute z-10 right-0 w-48 bg-white rounded-md shadow">
+          <div className="p-2 flex flex-col gap-2">
+            <span className="text-center font-medium">{user.user.name}</span>
+            <Button className="w-full" onClick={logOut}>
+              Log out
+            </Button>
+          </div>
+        </Popover.Panel>
+      </Popover>
+
+      {/*<Button onClick={logOut}>Logout</Button>*/}
+    </div>
   );
 }
