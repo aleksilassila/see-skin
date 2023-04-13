de5fbf441629a49dbcf77250d4e42e346cce6dd4
Aleksi Lassila <aleksi.emil.lassila@gmail.com>
2023-04-14 02:13:55 +0300

    Updated docker configuration and GitHub actions, test setup

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,7 @@
 import app from "./app";
 
 app.listen(9000, () => {
-  console.log("Server started on port 9000");
+  console.log(
+    "Server started on port 9000 in " + process.env.NODE_ENV + " mode"
+  );
 });
