f3f804dc7754c261c20af292a54359ea6c3797e1
Aleksi Lassila <aleksi.emil.lassila@gmail.com>
2023-05-11 13:33:21 +0300

    Build fix

--- frontend/app/account/manage-irritants/create-skin-profile-first.tsx
+++ frontend/app/account/manage-irritants/create-skin-profile-first.tsx
@@ -3,7 +3,7 @@ import { Button } from "../../(components)/ui/button";
 export function CreateSkinProfileFirst() {
   return (
     <div>
-      <div>You don't have a SkinProfile set up.</div>
+      <div>You don&apos;t have a SkinProfile set up.</div>
       <Button intent="primary">Create SkinProfile</Button>
     </div>
   );
