23a93955d1cdb2292465b96acbdddda384d9774f
Aleksi Lassila <aleksi.emil.lassila@gmail.com>
2023-06-07 19:13:16 +0300

    Account page mobile hotfix

--- frontend/app/account/layout.tsx
+++ frontend/app/account/layout.tsx
@@ -8,7 +8,7 @@ import { WithNavigation } from "../(navigation)/with-navigation";
 function AccountLayout(props: PropsWithChildren) {
   return (
     <div className="flex-1 flex justify-center p-4 md:p-16 relative">
-      <div className="flex-1 flex flex-col md:flex-row justify-center max-w-7xl gap-2 md:gap-8">
+      <div className="flex-1 flex flex-col md:flex-row justify-center max-w-7xl gap-2 md:gap-8 min-w-0">
         <AccountSidebar />
         <div className="flex-1 md:mx-auto md:max-w-md lg:max-w-xl xl:max-w-2xl">
           {props.children}
